9c5c034cc0bc84f3038b3f0fc62dde286dd367d1
praktykantdbir <praktykant_dbir@multiplay.pl>
2024-05-24 15:32:14 +0200

    we get connection status from api after sending the adress

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  runtimeConfig:{
+    mmsToken: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJodHRwczovL21tcy1kZXZlbG9wbWVudC5kZXYubXVsdGlwbGF5LnBsIiwiaWF0IjoxNzE2MjA2MTY2LCJleHAiOjE3NDc3NDIxNjYsIm5iZiI6MTcxNjIwNjE2NiwianRpIjoiT1lpQnV3UzhETkowOHpwMyIsInN1YiI6IjI1IiwicHJ2IjoiODdlMGFmMWVmOWZkMTU4MTJmZGVjOTcxNTNhMTRlMGIwNDc1NDZhYSIsInR5cGUiOiJ1c2VyIn0.c320EyOmMUjYnKjf7oOrtx3mdyflDdt5OYOd-rY9_rOnJtQRCoES3N_TdQYo5TY5WcXG0Yv6R1O0RQyM0oaj9A'
+  },
+  components: [
+    {
+      path: '@/components',
+      pathPrefix: false
+    },
+  ],
+  css: ['~/assets/css/main.css'],
+  // postcss: {
+  //   plugins: {
+  //     tailwindcss: {},
+  //     autoprefixer: {},
+  //   },
+  // },
+  modules: [
+    '@pinia/nuxt',
+  ],
+
+});
